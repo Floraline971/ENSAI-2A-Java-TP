7a3fd5efd35b7e26beb41e75b7d15bac98ad7694
floraline <floraline.cratere@eleve.ensai.fr>
2026-03-20 11:47:42 +0000

    maj TP3

---
+++ tp3/src/main/java/fr/ensai/mediaplayer/Artist.java
@@ -0,0 +1,5 @@
+public class Artist(String firstName, String lastName, String nationality){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
---
+++ tp3/src/main/java/fr/ensai/mediaplayer/Podcast.java
@@ -0,0 +1,15 @@
+public class Podcast (String title, String host, String topic, int duration, int year, String subtitles) {
+Podcast
+       this.title = title;
+        this.host = host;
+        this.topic = topic;
+        this.duration = duration;
+        this.year = year;
+        this.subtitles = subtitles;
+
+        
+play(title){
+    return 
+}    
+
+}
