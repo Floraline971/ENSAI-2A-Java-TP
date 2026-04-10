6d80f16da230e24f93fc79553afe820545b19ff7
floraline <floraline.cratere@eleve.ensai.fr>
2026-04-10 08:48:09 +0000

    ajout question 2

--- tp4/src/main/java/fr/ensai/elevator/Main.java
+++ tp4/src/main/java/fr/ensai/elevator/Main.java
@@ -17,6 +17,8 @@ public class Main {
         // --------------------------------------------------------------------
 
         final int elevatorCapacity = Config.getInt("hotel.elevator.capacity");
+        final int elevatorNumber = Config.getInt("hotel.elevator.number");
+        final int floorNumber = Config.getInt("hotel.floor.number");
         final int nbSteps = Config.getInt("steps.count");
         final int msBetweenSteps = Config.getInt("steps.delay");
 
@@ -25,6 +27,10 @@ public class Main {
         floors.add(new Floor(1));
         floors.add(new Floor(2));
         floors.add(new Floor(3));
+
+        for (int i = 1; i <= elevatorNumber; i++) {
+        floors.add(new Floor(i));
+        }  
 
         List<Elevator> elevators = new ArrayList<>();
         elevators.add(new Elevator(1, 0, elevatorCapacity));
@@ -43,6 +49,31 @@ public class Main {
             hotel.spawnPerson();
             hotel.display(step);
             Thread.sleep(msBetweenSteps);
+        }
+        
+
+        for (int step = 1; step <= nbSteps; step++) {
+            logger.info("\nStep: {}\n--------", step);
+
+            hotel.update();
+            hotel.spawnPerson();
+            hotel.display(step);
+            Thread.sleep(msBetweenSteps);
+        }
+        {
+            init: function(elevators, floors) {
+                var elevator = elevators[0]; // Let's use the first elevator
+
+                // Whenever the elevator is idle (has no more queued destinations) ...
+                elevator.on("idle", function() {
+                    // let's go to all the floors (or did we forget one?)
+                    elevator.goToFloor(0);
+                    elevator.goToFloor(1);
+                });
+            },
+            update: function(dt, elevators, floors) {
+                // We normally don't need to do anything here
+            }
         }
     }
 
